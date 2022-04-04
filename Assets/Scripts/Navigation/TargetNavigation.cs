02d2d7da1d80d2bcc0a154cb2b835da17f62ff0b
Jason Ellis <4539e.bluejayave@gmail.com>
2022-04-04 08:06:01 -0600

    AI Progress
    made a capsule follow a target disk, avoids walls and finds best route to take to get to the disk.

---
+++ Assets/Scripts/Navigation/TargetNavigation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetNavigation : MonoBehaviour {
+
+    [SerializeField] private Transform movePositionTransform;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>(); 
+    }
+
+    private void Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}

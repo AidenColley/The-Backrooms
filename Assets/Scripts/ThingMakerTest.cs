0b4eee9fdc10cffb0c4a62ed3f0420fa44448db2
Noah <potato.duck.face@gmail.com>
2022-04-07 09:25:40 -0600

    Prefab Setup
    Basic room design set.

---
+++ Assets/Scripts/ThingMakerTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingMakerTest : MonoBehaviour
+{
+
+/*
+ * stuff that could be helpful, but needs to be in awake
+ * 
+        Vector3 oldPos = room1.transform.localPosition;       
+        
+        for (int i = 0; i < 3; i++)
+        {
+
+            GameObject buh = Instantiate(room1);
+
+            buh.transform.localPosition = new Vector3((oldPos.x + 5), oldPos.y, oldPos.z);
+
+            oldPos = buh.transform.localPosition;
+        }
+        */
+
+    /// <summary>
+    /// make 4 arrays to grab door that CAN connect
+    /// arrays are gonna be for cardinal directions, if a doorway has north, grab from a south doorway array
+    /// </summary>
+
+
+    public GameObject room1;
+    public GameObject room2;
+    public GameObject room3;
+    public GameObject room4;
+    public GameObject room5;
+    public GameObject room6;
+    public GameObject room7;
+
+
+    void Start()
+    {
+        
+
+
+
+
+
+
+
+
+
+    }
+
+
+     
+
+
+
+    
+}
+
+
+

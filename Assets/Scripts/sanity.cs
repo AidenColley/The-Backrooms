bf26bf0e4c134efbca742df08e0c7c9b3d7e01a7
Aiden <aidenzcolley@gmail.com>
2022-04-07 09:48:34 -0600

    more sanity and vhs work

--- Assets/Scripts/sanity.cs
+++ Assets/Scripts/sanity.cs
@@ -5,31 +5,67 @@ using UnityEngine.UI;
 
 public class sanity : MonoBehaviour
 {
-    [SerializeField] private float MaxSanity = 100f;
+    public float MaxSanity = 100f;
+    private float AlmondWaterIncreaseSanity;
     [SerializeField] private float CurrentSanity = 100f;
     [SerializeField] private float NormalSanityDrain = 0.5f;
     [SerializeField] private Canvas PlayerCanvas = null;
     [SerializeField] private Image SanityBar = null;
-
+    [SerializeField] private Text text = null;
+    
+    public Transform camera;
+    [SerializeField] private float interactDist = 2f;
+    
     
     void Start()
     {
-        
+      
     }
 
     
     void Update()
     {
-        if(CurrentSanity > 0)
+        
+        
+        Interact();
+        if (CurrentSanity > 0)
         {
             UpdateStamina(CurrentSanity);
             CurrentSanity -= NormalSanityDrain * Time.deltaTime;
+           
            
         }
     }
 
     void UpdateStamina(float value)
     {
-        SanityBar.fillAmount = CurrentSanity / MaxSanity;
+        value = Mathf.Round(value);
+        text.text = value.ToString() + '%';
+        //SanityBar.fillAmount = value / MaxSanity;
+    }
+
+
+    void Interact()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(camera.position, camera.forward, out hit, interactDist)){
+            if (hit.collider.gameObject.tag == "Almond Water") 
+            {
+                
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    AlmondWaterPickup();
+                    Destroy(hit.transform.gameObject);
+                }
+
+            }
+        }
+    }
+
+    void AlmondWaterPickup()
+    {
+        AlmondWaterIncreaseSanity =  Mathf.Clamp(MaxSanity - CurrentSanity,0,50);
+        CurrentSanity += AlmondWaterIncreaseSanity;
+        
     }
 }
